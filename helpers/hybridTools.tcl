cccfd7311c4f7dc07ef26039b3a57300f68324aa
Daniel Weber <weberdk01@gmail.com>
2020-06-07 21:01:23 -0500

    Can now measure azimuthal angles and apply Z-free alignments.

--- helpers/hybridTools.tcl
+++ helpers/hybridTools.tcl
@@ -1,12 +1,25 @@
+# hybridTools.tcl
+# ---------------
+# A VMD function to align and analyze helix topology.
+# Written by D. K. Weber (last revised June 7 2020)
+#
+# Features
+# --------
+# - Align helix by center of mass without affecting tilt or rotation angles
+# - Option to not apply Z translation (if depth of insertion is important)
+# - Align helical segment along X axis (good for figures)
+# - Measure tilt and azimuthal angles
 
-
-proc align_helix { selection { args } } {
-    # Centers helix selection to origin  
+proc helix_topology { h_selection a_selection { args } } {
+    # Centers helix selection to origin. Aligns along Z. Measures tilt and azimuthal angle.
     #
     # Parameters
     # ----------
-    # selection: obj
-    #    VMD selection. The code below will automatically extract relevant info.
+    # h_selection: obj
+    #    VMD selection of helical segment. The code below will automatically extract relevant info.
+    # a_selection: obj
+    #    VMD selection of residue for which azimuthal angle is calculated (i.e., a CA atom usually).
+    #    If more than one atoms is selected, the angle will be determined from the center of mass.
     #
     # Optional flags
     # --------------
@@ -18,18 +31,20 @@ proc align_helix { selection { args } } {
     # -inside <term>
     #    <term>: N or C
     #       The helix is flipped so that <term> is on lower leaflet. Default: N.
-    #
-    # Outputs
-    # -------
-    # Aligns helical segment 
+    # -no_z
+    #    Do not apply a Z translation when aligning helices. Default: off.
+    # -out: <file name>
+    #    <file name>: Name of file to output tilt and azimuthal angles. Default: null.
 
     # Handle optional arguments
     set fix_tilt [argparse $args "-fix_tilt" 1 "null"]
     set invert [ switch_on $args "-invert" off ]
     set inside [argparse $args "-inside" 1 "N"]
+    set no_z [ switch_on $args "-no_z" off ]
+    set out [argparse $args "-out" 1 "null"]
     
     # Initialize sub-selections
-    set indices [lsort -integer [$selection get index]]
+    set indices [lsort -integer [$h_selection get index]]
     set half [expr int([llength $indices]/2)]
     set i 0
     set firsthalf []
@@ -39,15 +54,20 @@ proc align_helix { selection { args } } {
 	if {$i >= $half} { lappend secondhalf $index }
 	incr i
     }
-    set indices [$selection get index]
+    set indices [$h_selection get index]
     set s1 [atomselect top "index $firsthalf and name N C CA"]
     set s2 [atomselect top "index $secondhalf and name N C CA"]
     set sa [atomselect top "index $indices and name N C CA"]
     set sel_all [ atomselect top all ]
 
+    # Select region surrounding azi residue for local centroid
+    set frag [$a_selection get fragment]
+    set resid [$a_selection get resid]
+    set azi_region [atomselect top "fragment $frag and resid [expr $resid - 2] to [expr $resid +2] and name N C CA"]
+    
     # Statistics
     set tilt_angles []
-    
+    set azi_angles []
     # Loop through each frame and record helical angle
     set num_frames [molinfo top get numframes]
     for {set i 0} {$i < $num_frames} {incr i} {
@@ -56,74 +76,132 @@ proc align_helix { selection { args } } {
 	$sa frame $i
 	$sel_all frame $i
 	
-	# Measure center
+	# Measure center of masses for N-term (s1) and C-term (s2) helical segments
 	set c1 [measure center $s1]
 	set c2 [measure center $s2]
 
-	# Flip helix so that <term> in on lower leaflet
+	# Flip helix so that <term> in on lower leaflet (if not already)
 	if { $inside == "N" } {
 	    if { [lindex $c1 2] > [lindex $c2 2] } {
-		puts "bamn"
 		set rot [trans axis x 180 deg]
 		$sel_all move $rot
 	    }
 	}
 	if { $inside == "C" } {
 	    if { [lindex $c1 2] < [lindex $c2 2] } {
-		puts "bamc"
 		set rot [trans axis x 180 deg]
 		$sel_all move $rot
 	    }
 	}
 	
-	# Re-measure centers
-	set c1 [measure center $s1]
-	set c2 [measure center $s2]	    
-	
-	# Measure angle 
-        set vs [vecnorm [vecsub $c2 $c1]]
-	set ang [expr atan([lindex $vs 0]/[lindex $vs 1])]
-	
-	# Rotate so Y component is always positive
-	if { [lindex $vs 1] < 0 } {
-	    set rot [trans axis z [expr $ang + 3.1415926535897931] rad]
+	# Re-measure centers (apply inversion if reading tilt in C-N direction)
+	if { $invert == on } {
+	    #puts "Helix tilt angle will be measured in C to N direction"
+	    set c1 [measure center $s2]
+	    set c2 [measure center $s1]
 	} else {
-	    set rot [trans axis z $ang rad]
+	    #puts "Helix tilt angle will be measured in N to C direction"
+	    set c1 [measure center $s1]
+	    set c2 [measure center $s2]	    
+	}
+	
+	# Measure tilt angle between center of masses
+	set vt [vecnorm [vecsub $c2 $c1]]
+	set t_ang [expr acos([lindex $vt 2]) * 57.2957795130823]
+	#puts $t_ang
+	lappend tilt_angles $t_ang
+
+	# Align helix along X axis
+	set ang [expr atan([lindex $vt 1]/[lindex $vt 0])]
+	
+	# Rotate so X component is always positive
+	if { [lindex $vt 0] < 0 } {
+	    set rot [trans axis z [expr -$ang + 3.1415926535897931] rad]
+	} else {
+	    set rot [trans axis z -$ang rad]
 	}
 	$sel_all move $rot
 
-	# Measure helix angle
-	if { $invert == on } {
-	    set vt [vecnorm [vecsub $c1 $c2]]
-	} else {
-	    set vt [vecnorm [vecsub $c2 $c1]]
-	}
-	set t_ang [expr acos([lindex $vt 2]) * 57.2957795130823]
-	puts $t_ang
-	lappend tilt_angles $t_ang
-	
-	# Rotate to specified angle if desired
+	# Rotate to specified fixed angle if desired
 	if { $fix_tilt != "null" } {
-	    set rot [trans axis x [expr $fix_tilt - $t_ang] deg]
+	    set rot [trans axis y [expr $fix_tilt - $t_ang] deg]
 	    $sel_all move $rot
 	}
 	
 	# Translate center to origin
 	set ca [measure center $sa]
 	set trans [trans origin $ca]
+	if { $no_z == on } {
+	    puts "test"
+	    set xrow [lindex $trans 0]
+	    set yrow [lindex $trans 1]
+	    set zrow [lindex $trans 2]
+	    set zrow [list [lindex $zrow 0] [lindex $zrow 1] [lindex $zrow 2] 0.0 ]
+	    set irow [lindex $trans 3]
+	    set trans [list $xrow $yrow $zrow $irow]
+	}
+	puts $trans
 	$sel_all move $trans
+	draw sphere [measure center $sa]
 	
+	# Calculate azimuthal angle of residue
+	# Rotate tilt to zero and determine angle from X,Y displacement of azi point from centroid.
+	$a_selection frame $i
+	$azi_region frame $i
+	set azi_point [measure center $a_selection]
+	set c [measure center $azi_region]
+	set rot [trans axis y [expr 0 - $t_ang] deg]
+	# Rotate points along Z axis and meaure X and Y deviations
+	set c [vectrans $rot $c]
+	set azi_point [vectrans $rot $azi_point]
+	#draw color blue
+	#draw sphere $c
+	#draw color red
+	#draw sphere $azi_point
+	set diff [vecsub $azi_point $c]
+	set azi_y [expr atan([lindex $diff 0]/[lindex $diff 1]) * 57.2957795130823]
+	set azi_x [expr atan([lindex $diff 1]/[lindex $diff 0]) * 57.2957795130823]
+	
+	# Determine phase (Y = 180, -Y = 0, X = 90, -X = 270)
+	if { [lindex $diff 1] > 0 } {
+	    # Positive Y
+	    set azi [expr 180 - $azi_y]
+	} else {
+	    # Negative Y
+	    set azi [expr 360 + $azi_y]
+	}
+	lappend azi_angles $azi
+	#puts "$azi_y\t$azi_x\t$azi"	
     }
-    # Output
-    set t_ang_mean [average $tilt_angles] 
-    puts "Average tilt angle of segment: $t_ang_mean"
+    
+    # Output to terminal
+    set t_ang_mean [format "%.2f" [lindex [stats $tilt_angles] 0]]
+    set t_ang_std  [format "%.2f" [lindex [stats $tilt_angles] 1]]
+    set a_ang_mean [format "%.2f" [lindex [stats $azi_angles] 0]]
+    set a_ang_std  [format "%.2f" [lindex [stats $azi_angles] 1]]
+    puts "Average tilt angle of segment: $t_ang_mean +/- $t_ang_std"
+    puts "Average azi angle residue [lindex $resid 0]: $a_ang_mean +/- $a_ang_std"
+
+    # Output results to files
+    if { $out != "null" } {
+	puts "Outputting result to $out..."
+	set outf [open $out w]
+	puts $outf "#Frame\tTilt\tazi_[lindex $resid 0]"
+	for {set i 0} {$i < $num_frames} {incr i} {
+	    set tilt [format "%.2f" [lindex $tilt_angles $i]]
+	    set azi [format "%.2f" [lindex $azi_angles $i]]
+	    puts $outf "$i\t$tilt\t$azi"
+	}
+	close $outf
+    }
     
     # Clean up
     $s1 delete
     $s2 delete
     $sa delete
     $sel_all delete
-    unset s1 s2 sa sel_all
+    $azi_region delete
+    unset s1 s2 sa sel_all azi_region
 }
 
 
@@ -154,6 +232,7 @@ proc argparse { args flag input_index default } {
     return $value
 }
 
+
 # Proc to handle switched
 proc switch_on { args flag default } {
     set value $default
@@ -164,10 +243,29 @@ proc switch_on { args flag default } {
 }
 
 
-proc average { data } {
+proc stats { data } {
+    # Compute mean and standard deviation of list
+    #
+    # Parameters
+    # ----------
+    # data: list of floats
+    #    List of numbers.
+    #
+    # Returns
+    # -------
+    # [mean, std]: list of two floats
+    #    Mean and standard deviation.
+    
+    # Comute mean
     set csum 0
+    set csum2 0
+    set num [llength $data]
     foreach i $data {
 	set csum [expr $csum + $i]
+	set csum2 [expr $csum2 + pow($i,2)] 
     }
-    return [expr $csum / [llength $data]]
+    set mean [expr $csum / $num]
+    set std [expr sqrt((($num*$csum2)-pow($csum,2))/($num*($num-1)))]
+    return [list $mean $std]
 }
+
